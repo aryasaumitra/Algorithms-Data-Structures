bee0d2d33dd86819fbbab98bb105c4fb1c6e4510
Arya Saumitra <aryasaumitra@gmail.com>
2021-08-24 22:16:21 +0530

    Updated Linked List

--- LinkedList.py
+++ LinkedList.py
@@ -53,7 +53,9 @@ class LinkedList:
             print(travelPointer.data,end='->')
             travelPointer=travelPointer.next
 
-    def find(self,value):
+        print()
+
+    def __find(self,value):
 
         if self.head.next is not None:
             kindOfvalue=''
@@ -75,45 +77,69 @@ class LinkedList:
 
             if flag != -1:
 
-                print('\nFound '+str(value)+': Type '+str(kindOfvalue))
+                #print('\nFound '+self.__find(value)['value']+': Type '+self.__find(value)['type'])
 
                 return {
                     'value':value,
                     'type':kindOfvalue
                 }
             else:
-                print('\nValue Not found')
+                # print('\nValue Not found')
                 return -1
 
         else:
-            print('\nEmpty Linked List')
-            return -1
+            # print('\nEmpty Linked List')
+            return -2
 
-    def __deleteStart(self,value):
-        pass
+    def findNode(self,value):
 
-    def __deleteEnd(self,value):
-        pass
+        if self.__find(value)!=-2:
+            return print('\nFound '+str(self.__find(value)['value'])+' and Type '+str(self.__find(value)['type'])+'\n') if self.__find(value)!=-1 else print("Value not found")
+        else:
+            print("Empty Linked List")
+            
+    def __deleteStart(self):
+        currentNode=self.head.next
+        self.head.next=currentNode.next
+
+    def __deleteEnd(self):
+        currentNode=self.head.next
+
+        while currentNode.next is not None:
+            prevNode=currentNode
+            currentNode=currentNode.next
+
+        prevNode.next=None
+
 
     def __deleteMiddle(self,value):
-        pass
+        currentNode=self.head.next
+
+        while currentNode.data !=value:
+            prevNode=currentNode
+            currentNode=currentNode.next
+
+        prevNode.next=currentNode.next
 
     def deleteValue(self,value):
 
         if self.head.next is not None:
-            if self.find(value) !=-1:
-                findValue=self.find(value)
+            if self.__find(value) !=-1:
+                findValue=self.__find(value)
 
                 if findValue['type']=='Start':
-                    self.__deleteStart(value)
+                    self.__deleteStart()
+                    print('List After Deletion')
                     self.printNodes()
 
                 elif findValue['type']=='End':
-                    self.__deleteEnd(value)
+                    self.__deleteEnd()
+                    print('List After Deletion')
                     self.printNodes()
 
                 else:
                     self.__deleteMiddle(value)
+                    print('List After Deletion')
                     self.printNodes()
             else:
                 print('\nValue not Found')
@@ -121,16 +147,65 @@ class LinkedList:
             print('\nList is Empty')
 
     def __insertStart(self,value):
-        pass
 
-    def __insertMiddle(self,value):
-        pass
+        newNode=Node(value)
+        currentStart=self.head.next
+        newNode.next=currentStart
+        self.head.next=newNode
+
+
+    def __insertMiddle(self,value,after):
+        
+        newNode=Node(value)
+
+        travelpointer=self.head.next
+
+        while travelpointer.data != after:
+            travelpointer=travelpointer.next
+
+        newNode.next=travelpointer.next
+        travelpointer.next=newNode
 
     def __insertEnd(self,value):
-        pass
+        
+        newNode=Node(value)
 
-    def insertValue(self,value,location):
-        pass
+        travelPointer=self.head.next
+
+        while travelPointer.next is not None:
+            travelPointer=travelPointer.next
+
+        travelPointer.next=newNode
+
+    def insertValue(self,value,location,after=None):
+        
+        if location=='Start':
+            print('List Before Insertion')
+            self.printNodes()
+            self.__insertStart(value)
+            print("List after Insertion")
+            self.printNodes()
+
+        elif location == 'End':
+            print('List Before Insertion')
+            self.printNodes()
+            self.__insertEnd(value)
+            print("List after Insertion")
+            self.printNodes()
+
+        elif location == 'Mid':
+            if after == None or self.__find(after) == -1 or self.__find(after) == -2:
+                print('After value does not exist')
+                self.printNodes()
+            else:
+                print('List Before Insertion')
+                self.printNodes()
+                self.__insertMiddle(value,after)
+                print("List after Insertion")
+                self.printNodes()
+
+
+
 
 
 
@@ -142,7 +217,11 @@ linkedList=LinkedList(values)
 
 linkedList.printNodes()
 
-linkedList.find(4)
+linkedList.findNode(4)
 
-linkedList.find(7)
+linkedList.findNode(7)
+
+linkedList.deleteValue(6)
+
+linkedList.insertValue(6,'Mid',8)
                 
